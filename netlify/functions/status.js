20b835405f7cb85dc72946a9faded7b59d7dc89e
Hcosta <helderversalhes@gmail.com>
2026-03-11 13:14:49 +0100

    Refactor status function to use blobGet

--- netlify/functions/status.js
+++ netlify/functions/status.js
@@ -1,4 +1,4 @@
-const { getDeployStore } = require('@netlify/blobs');
+const { blobGet } = require('./blobs-helper');
 
 exports.handler = async (event) => {
   const headers = {
@@ -10,11 +10,10 @@ exports.handler = async (event) => {
   if (event.httpMethod === 'OPTIONS') return { statusCode: 200, headers, body: '' };
 
   try {
-    const store = getDeployStore('cadetes');
     const login = new URLSearchParams(event.rawQuery || '').get('login');
     if (!login) return { statusCode: 400, headers, body: JSON.stringify({ error: 'login em falta' }) };
 
-    const data = await store.get(login, { type: 'json' }).catch(() => null);
+    const data = await blobGet(login);
     if (!data) return { statusCode: 200, headers, body: JSON.stringify({ status: 'unknown' }) };
 
     return { statusCode: 200, headers, body: JSON.stringify({ status: data.status }) };
